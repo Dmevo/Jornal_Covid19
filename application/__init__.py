d0cd77f85e5876c1111effd665835800da04ceea
Danilo Vasconcellos <danilodemv@hotmail.com>
2021-04-29 21:51:05 -0300

    Alterações sendo feitas

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,10 @@ from application.model.entity.noticia import Noticia
 
 app = Flask(__name__, static_folder = os.path.abspath("application/view/static"), template_folder = os.path.abspath("application/view/templates"))
 
-noticia1 = Noticia("url_img", "Covid19 - 1", "texto1", "20/01/2021", 1)
-noticia2 = Noticia("url_img", "Covid19 - 2", "texto2", "13/02/2021", 2)
-noticia3 = Noticia("url_img", "Covid19 - 3", "texto3", "04/03/2020", 3)
-noticia4 = Noticia("url_img", "Covid19 - 4", "texto4", "08/01/2021", 4)
+noticia1 = Noticia("url_img", "Covid19 - 1", "texto1", "20/01/2021", 1, 19537835, 215463)
+noticia2 = Noticia("url_img", "Covid19 - 2", "texto2", "13/02/2021", 2, 82734657, 363456)
+noticia3 = Noticia("url_img", "Covid19 - 3", "texto3", "04/03/2020", 3, 76354743, 321542)
+noticia4 = Noticia("url_img", "Covid19 - 4", "texto4", "08/01/2021", 4, 62643566, 677265)
 
 noticia_lista = [noticia1, noticia2, noticia3, noticia4]
 
--- application/model/entity/noticia.py
+++ application/model/entity/noticia.py
@@ -1,11 +1,13 @@
 class Noticia:
 
-    def __init__(self, url_img = None, titulo = None, texto = None, data_publicacao = None, id = None):
+    def __init__(self, url_img = None, titulo = None, texto = None, data_publicacao = None, id = None, visualizacao = None, curtidas = None):
         self.url_img = url_img
         self.titulo = titulo
         self.texto = texto
         self.data_publicacao = data_publicacao
         self.id = id
+        self.visualizacao = visualizacao
+        self.curtidas = curtidas
 
     def get_url_img(self):
         return self.url_img
@@ -21,6 +23,12 @@ class Noticia:
 
     def get_id(self):
         return self.id
+
+    def get_visualizacao(self):
+        return self.visualizacao
+
+    def get_curtidas(self):
+        return self.curtidas
 
 
 '''
