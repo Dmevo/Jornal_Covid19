40cc9ce087045ec2b4dd4ab7dcafedee35bb78ee
Danilo Vasconcellos <danilodemv@hotmail.com>
2021-05-01 16:33:36 -0300

    Atualizando noticia.py

--- application/model/entity/noticia.py
+++ application/model/entity/noticia.py
@@ -1,6 +1,6 @@
 class Noticia:
 
-    def __init__(self, url_img = None, titulo = None, texto = None, data_publicacao = None, id = None, visualizacao = None, curtidas = None):
+    def __init__(self, url_img = None, titulo = None, texto = None, data_publicacao = None, id = None, visualizacao = None, curtidas = None, url_noticia = None):
         self.url_img = url_img
         self.titulo = titulo
         self.texto = texto
@@ -8,6 +8,7 @@ class Noticia:
         self.id = id
         self.visualizacao = visualizacao
         self.curtidas = curtidas
+        self.url_noticia = url_noticia
 
     def get_url_img(self):
         return self.url_img
@@ -29,6 +30,12 @@ class Noticia:
 
     def get_curtidas(self):
         return self.curtidas
+
+    def get_url_noticia(self):
+        return self.url_noticia
+
+    def set_url_noticia(self, url_noticia):
+        self.url_noticia = url_noticia
     
     def set_texto(self, texto):
         self.texto = texto
